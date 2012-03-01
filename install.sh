c9c340ca2662b3a95c3ed79fda4023e9a8dd6844
Kei Kubo <keikubo@gmail.com>
2012-03-01 01:33:15 +0000

    bug fix: modified install.sh to avoid cp errors

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 cd /tmp
 git clone git://github.com/keikubo/rc.git
-cp -afp /tmp/rc/home/.* ~/
+cp -afp /tmp/rc/home/.vim* ~/
+cp -afp /tmp/rc/home/.git* ~/
+cp -afp /tmp/rc/home/.zsh* ~/
+cp -afp /tmp/rc/home/.screen* ~/
+rm -rf /tmp/rc
